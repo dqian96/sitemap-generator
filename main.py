f9e7d680c15481e6e202e3a01a2697575c7c8a89
David Qian <davidqian1996@gmail.com>
2016-11-25 19:49:39 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,8 @@ import web_crawler
 
 #main program
 print
-print "Hey! Welcome to Web Crawler/Sitemap Generator by David Qian."
-print "This product is licensed under the MIT license."
-print "No warranty is provided of any kind nor gurantee of proper function."
-print "Please use this product for non-evil purposes."
-print "Some sites will deny to be crawled because of their robots.txt document. Please respect their wishes!"
+print "Welcome to Web Crawler/Sitemap Generator."
+print "Some sites will deny to be crawled because of their robots.txt. Please respect their wishes!"
 print
 
 root_URL = raw_input("Enter the EXACT, ROOT URL of the site you wish to crawl (0 to exit): ")
